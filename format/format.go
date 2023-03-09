d63d36cf7c50e64f3f347d77b1be408244b2768e
nemo <nemo1002forwork@gmail.com>
2023-03-09 21:42:01 +0800

    add format

---
+++ format/format.go
@@ -0,0 +1,37 @@
+package format
+
+import (
+	"bytes"
+	"encoding/json"
+	"text/template"
+)
+
+func GoTempate(format string, data any) (string, error) {
+	tmpl := template.New("bh").Funcs(builtins())
+	tmpl, err := tmpl.Parse(format)
+	if err != nil {
+		return "", err
+	}
+
+	buf := &bytes.Buffer{}
+	err = tmpl.Execute(buf, data)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf.Bytes()), nil
+}
+
+func builtins() template.FuncMap {
+	return template.FuncMap{
+		"json": jsonFormat,
+	}
+}
+
+func jsonFormat(v interface{}) (string, error) {
+	result, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+}
