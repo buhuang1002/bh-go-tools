9a6a37145b252022de4a3433d9a27c8677a45295
nemo <nemo1002forwork@gmail.com>
2024-11-14 10:52:15 +0800

    add bhmath module

---
+++ bhmath/bhbig/int.go
@@ -0,0 +1,181 @@
+package bhbig
+
+import (
+	"fmt"
+	"math/big"
+)
+
+// BigIntMaxSerializedLen is the max length of a byte slice representing a CBOR serialized big.
+const BigIntMaxSerializedLen = 128
+
+func NewInt(i int64) *big.Int {
+	return big.NewInt(0).SetInt64(i)
+}
+
+func NewIntUnsigned(i uint64) *big.Int {
+	return big.NewInt(0).SetUint64(i)
+}
+
+func NewFromGo(i *big.Int) *big.Int {
+	return big.NewInt(0).Set(i)
+}
+
+func Zero() *big.Int {
+	return NewInt(0)
+}
+
+// PositiveFromUnsignedBytes interprets b as the bytes of a big-endian unsigned
+// integer and returns a positive Int with this absolute value.
+func PositiveFromUnsignedBytes(b []byte) *big.Int {
+	i := big.NewInt(0).SetBytes(b)
+	return i
+}
+
+// MustFromString convers dec string into big integer and panics if conversion
+// is not sucessful.
+func MustFromString(s string) *big.Int {
+	v, err := FromString(s)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func FromString(s string) (*big.Int, error) {
+	v, ok := big.NewInt(0).SetString(s, 10)
+	if !ok {
+		return nil, fmt.Errorf("failed to parse string as a big int")
+	}
+
+	return v, nil
+}
+
+func Product(ints ...*big.Int) *big.Int {
+	p := NewInt(1)
+	for _, i := range ints {
+		p = Mul(p, i)
+	}
+	return p
+}
+
+func Mul(a, b *big.Int) *big.Int {
+	return big.NewInt(0).Mul(a, b)
+}
+
+func Div(a, b *big.Int) *big.Int {
+	return big.NewInt(0).Div(a, b)
+}
+
+func Mod(a, b *big.Int) *big.Int {
+	return big.NewInt(0).Mod(a, b)
+}
+
+func Add(a, b *big.Int) *big.Int {
+	return big.NewInt(0).Add(a, b)
+}
+
+func Sum(ints ...*big.Int) *big.Int {
+	sum := Zero()
+	for _, i := range ints {
+		sum = Add(sum, i)
+	}
+	return sum
+}
+
+func Subtract(num1 *big.Int, ints ...*big.Int) *big.Int {
+	sub := num1
+	for _, i := range ints {
+		sub = Sub(sub, i)
+	}
+	return sub
+}
+
+func Sub(a, b *big.Int) *big.Int {
+	return big.NewInt(0).Sub(a, b)
+}
+
+// Returns a**e unless e <= 0 (in which case returns 1).
+func Exp(a *big.Int, e *big.Int) *big.Int {
+	return big.NewInt(0).Exp(a, e, nil)
+}
+
+// Returns x << n
+func Lsh(a *big.Int, n uint) *big.Int {
+	return big.NewInt(0).Lsh(a, n)
+}
+
+// Returns x >> n
+func Rsh(a *big.Int, n uint) *big.Int {
+	return big.NewInt(0).Rsh(a, n)
+}
+
+func BitLen(a *big.Int) uint {
+	return uint(a.BitLen())
+}
+
+func Max(x, y *big.Int) *big.Int {
+	if Equals(x, Zero()) && Equals(x, y) {
+		if x.Sign() != 0 {
+			return y
+		}
+		return x
+	}
+
+	if GreaterThan(x, y) {
+		return x
+	}
+	return y
+}
+
+func Min(x, y *big.Int) *big.Int {
+	if Equals(x, Zero()) && Equals(x, y) {
+		if x.Sign() != 0 {
+			return x
+		}
+		return y
+	}
+
+	if LessThan(x, y) {
+		return x
+	}
+	return y
+}
+
+func Cmp(a, b *big.Int) int {
+	return a.Cmp(b)
+}
+
+func Equals(a, b *big.Int) bool {
+	return Cmp(a, b) == 0
+}
+
+func GreaterThan(a, b *big.Int) bool {
+	return Cmp(a, b) > 0
+}
+
+func LessThan(a, b *big.Int) bool {
+	return Cmp(a, b) < 0
+}
+
+func FromBytes(buf []byte) (*big.Int, error) {
+	if len(buf) == 0 {
+		return NewInt(0), nil
+	}
+
+	var negative bool
+	switch buf[0] {
+	case 0:
+		negative = false
+	case 1:
+		negative = true
+	default:
+		return Zero(), fmt.Errorf("big int prefix should be either 0 or 1, got %d", buf[0])
+	}
+
+	i := big.NewInt(0).SetBytes(buf[1:])
+	if negative {
+		i.Neg(i)
+	}
+
+	return i, nil
+}
