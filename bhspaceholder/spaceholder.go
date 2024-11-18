2381cdc15c4581ce989efe847f038c718fe72e45
nemo <nemo1002forwork@gmail.com>
2024-11-18 11:11:31 +0800

    add spaceholder module

---
+++ bhspaceholder/spaceholder.go
@@ -0,0 +1,81 @@
+package bhspaceholder
+
+func Bool() bool {
+	panic("unimplemented bool")
+}
+
+func Uint8() uint8 {
+	panic("unimplemented uint8")
+}
+
+func Uint16() uint16 {
+	panic("unimplemented uint16")
+}
+
+func Uint32() uint32 {
+	panic("unimplemented uint32")
+}
+
+func Uint64() uint64 {
+	panic("unimplemented uint64")
+}
+
+func Int8() int8 {
+	panic("unimplemented int8")
+}
+
+func Int16() int16 {
+	panic("unimplemented int16")
+}
+
+func Int32() int32 {
+	panic("unimplemented int32")
+}
+
+func Int64() int64 {
+	panic("unimplemented int64")
+}
+
+func Float32() float32 {
+	panic("unimplemented float32")
+}
+
+func Float64() float64 {
+	panic("unimplemented float64")
+}
+
+func Complex64() complex64 {
+	panic("unimplemented complex64")
+}
+
+func Complex128() complex128 {
+	panic("unimplemented complex128")
+}
+
+func String() string {
+	panic("unimplemented string")
+}
+
+func Int() int {
+	panic("unimplemented int")
+}
+
+func Uint() uint {
+	panic("unimplemented uint")
+}
+
+func Uintptr() uintptr {
+	panic("unimplemented uintptr")
+}
+
+func Byte() byte {
+	panic("unimplemented byte")
+}
+
+func Rune() rune {
+	panic("unimplemented rune")
+}
+
+func Any() any {
+	panic("unimplemented any")
+}
